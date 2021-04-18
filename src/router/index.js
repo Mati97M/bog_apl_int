b669aac35eea728f89701ddc13513be90662cfde
Mati97M <80071420+Mati97M@users.noreply.github.com>
2021-04-18 15:33:25 +0200

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import Student from '../views/Student.vue';
+import Chart from '../views/Chart.vue';
+import Grades from '../views/Grades.vue';
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/student',
+    name: 'Student',
+    component: Student,
+  },
+
+  {
+    path: '/chart',
+    name: 'Chart',
+    component: Chart,
+  },
+
+  {
+    path: '/grades',
+    name: 'Grades',
+    component: Grades,
+  }
+
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
